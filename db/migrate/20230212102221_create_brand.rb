9c9e7fde2050dcdae41f8f283f73e512f3dfa17f
Juan Andrew <juandreww108@gmail.com>
2023-02-12 17:26:17 +0700

    llll

--- db/migrate/20230212102221_create_brand.rb
+++ db/migrate/20230212102221_create_brand.rb
@@ -1,6 +1,10 @@
 class CreateBrand < ActiveRecord::Migration[7.0]
   def change
     create_table :brands do |t|
+      t.string :name
+      t.string :address
+      t.integer :owner_id
+      t.name :owner_name
 
       t.timestamps
     end
