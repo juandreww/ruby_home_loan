793dc9fcc93bf2bc46261141d04ee1e41dd7a713
Juan Andrew <juandreww108@gmail.com>
2023-02-15 22:29:34 +0700

    staged change

--- db/migrate/20230212102544_create_owner.rb
+++ db/migrate/20230212102544_create_owner.rb
@@ -1,7 +1,11 @@
 class CreateOwner < ActiveRecord::Migration[7.0]
   def change
     create_table :owners do |t|
-      t.string :name
+      t.string :email, null: false
+      t.string :name, null: false
+      t.string :password, null: false
+      t.string :phone, null: false
+      t.string :user_uuid, null: false
 
       t.timestamps
     end
