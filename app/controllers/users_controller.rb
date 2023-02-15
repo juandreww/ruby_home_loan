24ff8c3eb048f287b18b59621df3367145886698
Juan Andrew <juandreww108@gmail.com>
2023-02-16 05:58:39 +0700

    asdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def sign_up
+    byebug
   end
 
   def sign_in
+    byebug
   end
 
   def successful_sign_up
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_133644) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_152057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_133644) do
   end
 
   create_table "owners", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.string "phone", null: false
+    t.string "user_uuid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
