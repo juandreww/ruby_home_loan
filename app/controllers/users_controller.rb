a523c90f4b24b038f6395e632701c21a0da32ae2
Juan Andrew <juandreww@outlook.com>
2023-02-25 07:26:32 +0700

    send pin worker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def sign_up
     @user = User.new
   end
--- app/workers/send_pin_worker.rb
+++ app/workers/send_pin_worker.rb
@@ -2,15 +2,18 @@ class SendPinWorker
   include Sidekiq::Worker
 
   def perform(phone)
-    accounts_sid = ENV['ACCOUNT_SID']
+    account_sid = ENV['ACCOUNT_SID']
     auth_token = ENV['AUTH_TOKEN']
     service_sid = ENV['SERVICE_SID']
 
     client = Twilio::REST::Client.new(account_sid, auth_token)
-    verification_service = client.verify.services(service_sid)
 
-    verification_service
-      .verifications
-      .create(to: phone, channel: 'sms')
+    message = client.messages.create(
+      body: 'Thank you for signing up !',
+      messaging_service_sid: service_sid,
+      to: phone
+    )
+
+    puts message.sid
   end
 end
