b18dd0ea25c316cd91a2faa3a62deef2c05eb664
Juan Andrew <juandreww@outlook.com>
2023-03-05 08:37:33 +0700

    home loan calculate spec

--- spec/controller/home_loans_request_spec.rb
+++ spec/controller/home_loans_request_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe HomeLoansController, type: :request do
     expect(response.status).to eql 302
     expect(session['flash']['flashes']).to eql({"notice"=>"Amount you have to pay per month is IDR 916,667.00"})
   end
+
+  it 'should return valid calculation' do
+    post '/home_loans/calculate', params: { amount: 100_000_000, term_in_years: 4, monthly_interest_rate: 8 }
+
+    expect(response.status).to eql 302
+    expect(session['flash']['flashes']).to eql({"notice"=>"Amount you have to pay per month is IDR 2,250,000.00"})
+  end
+
+  it 'should return valid calculation' do
+    post '/home_loans/calculate', params: { amount: 250_000_000, term_in_years: 4, monthly_interest_rate: 8 }
+
+    expect(response.status).to eql 302
+    expect(session['flash']['flashes']).to eql({"notice"=>"Amount you have to pay per month is IDR 5,625,000.00"})
+  end
+
+  it 'should return valid calculation' do
+    post '/home_loans/calculate', params: { amount: 105_000_000_000, term_in_years: 8, monthly_interest_rate: 8 }
+
+    expect(response.status).to eql 302
+    expect(session['flash']['flashes']).to eql({"notice"=>"Amount you have to pay per month is IDR 1,181,250,000.00"})
+  end
 end
