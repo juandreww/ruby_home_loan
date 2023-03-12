fcf50c24f2d4f7816a1a1ca1678f6ad9beee71c4
Juan Andrew <juandreww@outlook.com>
2023-03-12 22:29:48 +0700

    updated successfully

--- app/controllers/admin/profile_controller.rb
+++ app/controllers/admin/profile_controller.rb
@@ -18,6 +18,22 @@ class Admin::ProfileController < ApplicationController
   end
 
   def update
+    if !filtered_user
+      redirect_to '/users/sign_in', notice: "Please login first"
+      return
+    end
+
+    draft_user = filtered_user
+    draft_user.assign_attributes(update_params)
+
+    if draft_user.errors.any?
+      render :edit, status: :unprocessable_entity
+      return
+    end
+
+    draft_user.save!
+
+    redirect_to "/admin/profile/#{draft_user.id}", notice: "Updated successfully"
   end
 
   def page_title
@@ -28,5 +44,9 @@ class Admin::ProfileController < ApplicationController
 
   def filtered_user
     @user = User.find_by(id: params[:id])
+  end
+
+  def update_params
+    update_params ||= params.require(:user).permit(:name)
   end
 end
