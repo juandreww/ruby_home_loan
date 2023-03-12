53057b0aea7c9a118ad14b40476bed5244aa3b0a
Juan Andrew <juandreww@outlook.com>
2023-03-12 19:41:37 +0700

    download profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,4 +2,14 @@ class ProfileController < ApplicationController
   def download
     send_data(Prawn::Document.new)
   end
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
