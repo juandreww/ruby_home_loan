b8c49c510ca48f4c9677eaab7a31f76e38bf3b01
Juan Andrew <juandreww@outlook.com>
2023-03-14 07:39:11 +0700

    asdfasdf

--- db/migrate/20230313004413_create_users_followers.rb
+++ db/migrate/20230313004413_create_users_followers.rb
@@ -1,7 +1,9 @@
 class CreateUsersFollowers < ActiveRecord::Migration[7.0]
   def change
     create_table :users_followers do |t|
-      
+      t.belongs_to :user, null: false, foreign_key: true
+      t.datetime :followed_at
+
       t.timestamps
     end
   end
