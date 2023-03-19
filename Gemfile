9274d9078f1afd3d6b89298aa28557fd6a077ef1
Juan Andrew <juandreww@outlook.com>
2023-03-19 09:33:03 +0700

    add image to users

--- Gemfile
+++ Gemfile
@@ -62,6 +62,8 @@ gem 'prawn-rails'
 gem 'prawn'
 gem 'zeitwerk'
 gem 'devise', git: 'https://github.com/plataformatec/devise'
+gem 'carrierwave'
+gem 'bootstrap-sass'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_141143) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_023153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_141143) do
     t.datetime "updated_at", null: false
     t.string "otp"
     t.integer "status", null: false
+    t.string "image"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
