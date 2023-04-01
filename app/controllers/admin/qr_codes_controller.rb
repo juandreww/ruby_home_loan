e5423076066bec00f40c85019af5efc0885aa9d6
Juan Andrew <juandreww@outlook.com>
2023-04-01 16:14:58 +0700

    qr codes

--- app/controllers/admin/qr_codes_controller.rb
+++ app/controllers/admin/qr_codes_controller.rb
@@ -6,5 +6,10 @@ class Admin::QrCodesController < ApplicationController
   end
 
   def download
+    send_data RQRCode::QRCode.new(params[:content].to_s).as_png(size: 300), type: 'image/png', disposition: 'attachment'
+  end
+
+  def page_title
+    'QR Codes'
   end
 end
