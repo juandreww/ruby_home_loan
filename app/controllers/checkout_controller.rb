25a00f78ca9926ac0c7c80d49a4ce9037955d504
Juan Andrew <juandreww@outlook.com>
2023-04-09 07:20:52 +0700

    checkout controller

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def create
+    session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'],
+      line_items: [
+        price_data: {
+          product: '{{PRODUCT_ID}}',
+          unit_amount: 1500,
+          currency: 'sgd',
+        },
+        quantity: 1,
+      ],
+      mode: 'payment',
+      success_url: 'https://example.com/success',
+      cancel_url: 'https://example.com/cancel',
+    })
   end
 end
