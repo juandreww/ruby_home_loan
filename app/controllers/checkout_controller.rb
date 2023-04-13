3de61076da24e5e62afa362cd6c2ce2d7a7e69ac
Juan Andrew <juandreww@outlook.com>
2023-04-13 21:56:16 +0700

    vcc

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,7 +3,7 @@ class CheckoutController < ApplicationController
     product = Product.find_by(id: params[:id])
 
     @session = Stripe::Checkout::Session.create({
-      payment_method_types: ['card'],
+      payment_method_types: ['vcc'],
       line_items: [
         price_data: {
           unit_amount: product.price.to_i,
