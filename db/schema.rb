f8377f0206e636d072aba5e334ed40b0174cf8db
Juan Andrew <juandreww@outlook.com>
2023-04-15 22:16:48 +0700

    show product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_000631) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_150237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -159,6 +159,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_000631) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "users", force: :cascade do |t|
