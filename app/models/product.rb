158ebbeec20633021333d9b36dfa7d9542f5e7a2
Juan Andrew <juandreww@outlook.com>
2023-04-21 14:48:11 +0700

    asdfasdf

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,11 @@ class Product < ApplicationRecord
   has_many_attached :images
 
   validates :name, :price, presence: true
-  validates :avatar, attached: true, content_type: 'image/png'
+  validates :avatar, attached: true,
+                     content_type: 'image/png',
+                     size: { less_than: 100.kilobytes ,
+                             message: 'cannot exceed 100kb' },
+                     aspect_ratio: :landscape
 
   def to_s
     name
