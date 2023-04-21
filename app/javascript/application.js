a0ee3a156d0c6de3d3ffcc68f026311f25df3bef
Juan Andrew <juandreww@outlook.com>
2023-04-21 13:07:27 +0700

    rails UJS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,8 +4,8 @@ import "controllers"
 import Rails from "@rails/ujs"
 
 Rails.start()
-// import * as ActiveStorage from "@rails/activestorage"
 
+// import * as ActiveStorage from "@rails/activestorage"
 // ActiveStorage.start()
 //= require jquery3
 //= require popper
